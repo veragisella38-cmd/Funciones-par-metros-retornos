6862325607d4c0adfd4f4abac7eb29e27c94748f
veragisella38-cmd <veragisella38@gmail.com>
2026-09-16 22:12:37 -0500

    Add files via upload

---
+++ tarea-funciones.py
@@ -0,0 +1,11 @@
+# Función que calcula el área de un rectángulo
+def calcular_area(base, altura):
+    # Retorna el resultado usando return
+    return base * altura
+
+# Programa principal
+# Llamada a la función con parámetros
+resultado = calcular_area(10, 5)
+
+# Mostrar el resultado en pantalla
+print("El área del rectángulo es:", resultado)
